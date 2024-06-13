f8ea649886490d93d0f7c67728541904bfe6238b
Nguyen Ngoc <134210774+ntngocccc@users.noreply.github.com>
2024-06-13 11:36:48 +0700

    update TicketManagementTest

--- src/test/java/test/admin/TicketManagementTest.java
+++ src/test/java/test/admin/TicketManagementTest.java
@@ -1,14 +1,34 @@
 package test.admin;
 
 import core.BaseTest;
+import helpers.RandomHelper;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import page.BookTicketPage;
+import page.CancelTicketPage;
+import page.admin.TicketManagementPage;
+
+import java.io.IOException;
 
 public class TicketManagementTest extends BaseTest {
+
+    TicketManagementPage page;
     BookTicketPage bookTicketPage;
+    @BeforeClass
+    public void setUp() throws IOException {
+        page = new TicketManagementPage();
+        bookTicketPage = new BookTicketPage();
+    }
 
     @Test(priority = 1, description = "Change ticket status successfully")
-    public void TC01() {
+    public void TC01_ChangeTicketStatus() throws InterruptedException {
+        loginPage.login("khachhang", "khachhang");
+        bookTicketPage.placeBooking(RandomHelper.randomFutureDate(), "", false);
+        page.logout();
+        loginPage.loginWithClick("thaigiavuong", "admin");
 
+        TicketManagementPage ticketManagementPage = new TicketManagementPage();
+        ticketManagementPage.changeTicketStatus();
     }
 }
